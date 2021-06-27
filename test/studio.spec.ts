a2ccb576adcba4688a83461426584217997336ce
Aratramba <aratramba@gmail.com>
2021-06-27 21:00:15 +0200

    add test timeout

--- test/studio.spec.ts
+++ test/studio.spec.ts
@@ -20,9 +20,13 @@ async function setCookies(context) {
 }
 
 test.describe("Studio", () => {
-  test("add movie", async ({ page, context }) => {
+  test.beforeEach(async ({ page, context }) => {
+    await page.waitForTimeout(5000);
     await setCookies(context);
     await page.goto(DOMAIN);
+  });
+
+  test("add movie", async ({ page }) => {
     await page.click("text=Movie");
     await page.click('[title="Create new Movie"]');
     await page.click('label:has-text("Title")');
@@ -31,15 +35,15 @@ test.describe("Studio", () => {
     expect(await page.isVisible('[class*="PaneItem_"]:has-text("Test movie")'));
   });
 
-  test("remove movie", async ({ page, context }) => {
-    await setCookies(context);
-    await page.goto(DOMAIN);
-    await page.click("text=Movie");
+  
+  test("remove movie", async ({ page }) => {
     await page.click("text=Test movie");
     await page.click('[aria-label="Actions"]');
     await page.click('[aria-label="Delete"]');
-    await page.click("text=Delete now");
+    await page.click('text=Delete now');
     expect(await page.isHidden('[class*="PaneItem_"]:has-text("Test movie")'));
     // await expect((await page.$$('[class*="PaneItem_"]:has-text("Test movie")')).length).toBe(0);
   });
+
+
 });
